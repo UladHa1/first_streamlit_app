19da77e829d01495799df7117614dc1c1459b616
UladHa1 <128421160+UladHa1@users.noreply.github.com>
2023-03-21 22:16:47 +0100

    Update streamlit_app.py
    fixed list added header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,10 @@ import streamlit
 
 streamlit.title('My mom\'s new healthy dinner')
 
-streamlit.header('🥣 Breakfast Menu')
-streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣  Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toasts')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
